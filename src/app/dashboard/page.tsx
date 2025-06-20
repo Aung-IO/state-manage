ce732c42f0b8f66cdd96f290720afe0ddc070b0f
Aung-IO <apkdust@gmail.com>
2025-06-21 06:03:52 +0630

    auth setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { logout } from '@/store/slices/authSlice'
+import { RootState } from '@/store/store'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+export default function DashboardPage() {
+    const user = useSelector((state: RootState) => state.auth.user)
+    const router = useRouter()
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        if (!user) {
+            router.push('/')
+        }
+    }, [user, router])
+
+    if (!user) return null
+
+    const handleLogout = () => {
+        dispatch(logout())
+        router.push('/')
+    }
+
+    return (
+        <div className='flex flex-col items-center justify-center min-h-screen'>
+            <h1 className='text-2xl font-bold'>Welcome, {user}</h1>
+            <button onClick={handleLogout} className='bg-blue-500 text-white p-2 rounded'>Logout</button>
+        </div>
+    )
+}
